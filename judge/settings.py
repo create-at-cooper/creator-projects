1b81f958fa62b7998784c214a5bc4dad3892d7b3
Eric Leong <leong@cooper.edu>
2012-10-14 23:17:54 -0400

    Revert back to debug/testing.

--- judge/settings.py
+++ judge/settings.py
@@ -31,10 +31,6 @@ if os.environ.get('LOCAL_DEV', 'True') == 'False':
             'BACKEND': 'django_pylibmc.memcached.PyLibMCCache'
         }
     }
-    
-    DEBUG = False
-    
-    STATIC_ROOT = '/app/dj/static/'
 else:
     DATABASES = {
         'default': {
@@ -47,11 +43,11 @@ else:
         }
     }
     
-    # Absolute path to the directory static files should be collected to.
-    # Don't put anything in this directory yourself; store your static files
-    # in apps' "static/" subdirectories and in STATICFILES_DIRS.
-    # Example: "/home/media/media.lawrence.com/static/"
-    STATIC_ROOT = ''
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = ''
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
